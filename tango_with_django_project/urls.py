75ff8bfca20742c6c088c1a9157873cacc23c119
ystttt <huiyizhilv1997@foxmail.com>
2021-07-31 18:06:19 +0100

    IT

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,5 +1,4 @@
 """tango_with_django_project URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
@@ -17,9 +16,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
-urlpatterns = [
-path('', views.index, name='index'),
-path('rango/', include('rango.urls')),
+from django.conf import settings
+from django.conf.urls.static import static
 
-path('admin/', admin.site.urls),
-]
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('rango/', include('rango.urls')),
+    # 3 - The above maps any URLs starting with rango/ to be handled by rango.
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
